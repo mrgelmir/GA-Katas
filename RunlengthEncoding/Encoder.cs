c1346ebf27528638a9075c8a82f13c707dd830c1
mrgelmir <sanderdecleer@gmail.com>
2024-04-29 11:38:29 +0200

    Refactored to StringBuilder

--- RunlengthEncoding/Encoder.cs
+++ RunlengthEncoding/Encoder.cs
@@ -1,14 +1,16 @@
 ﻿
+using System.Text;
+
 namespace RunlengthEncoding;
 
 public class Encoder
 {
     public string Encode(string input)
     {
-        string output = "";
+        StringBuilder output = new();
 
         if (input.Length <= 0)
-            return output;
+            return "";
 
         int counter = 0;
         char current = input[0];
@@ -23,20 +25,22 @@ public class Encoder
             }
             else
             {
-                output += counter.ToString() + current;
+                output.Append(counter);
+                output.Append(current);
                 counter = 1;
                 current = c;
             }
         }
 
-        output += counter.ToString() + current;
+        output.Append(counter.ToString());
+        output.Append(current);
 
-        return output;
+        return output.ToString();
     }
 
     public string Decode(string input)
     {
-        string output = "";
+        StringBuilder output = new();
         string currentNumber = "";
 
         for (int i = 0; i < input.Length; ++i)
@@ -50,11 +54,12 @@ public class Encoder
             else
             {
                 int amount = int.Parse(currentNumber);
-                output += new string(input[i], amount);
+                output.Append(input[i], amount);
                 currentNumber = "";
             }
         }
-        return output;
+
+        return output.ToString();
     }
 
 }
