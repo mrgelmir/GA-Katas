3aacd74bd74e8085e9774963da370f970207f45c
Sander Decleer <sanderdecleer@gmail.com>
2024-05-08 10:43:29 +0200

    Subarray sum initial solution

---
+++ SubarraySumEquals.Tests/SubarraySumTests.cs
@@ -0,0 +1,90 @@
+namespace SubarraySumEquals.Tests;
+
+public class SubarraySumTests
+{
+    [Fact]
+    public void EmptyInput_Find_ShouldReturnEmptyResult()
+    {
+        const int anyNumber = 10;
+
+        List<int> result = SubarraySum.Find([], anyNumber);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void SingleMatchingInput_Find_ShouldReturnSingleOutput()
+    {
+        const int number = 5;
+
+        List<int> result = SubarraySum.Find([number], number);
+
+        Assert.Single(result);
+        Assert.Equal(number, result[0]);
+    }
+
+    [Fact]
+    public void SingleNonMatchingInput_Find_ShouldReturnEmptyResult()
+    {
+        const int number = 5;
+        const int otherNumber = 3;
+
+        List<int> result = SubarraySum.Find([otherNumber], number);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void DoubleMatchingInput_Find_ShouldReturnCollection()
+    {
+        const int number = 5;
+        List<int> array = [2, 3];
+
+        List<int> result = SubarraySum.Find(array, number);
+
+        Assert.Equal(result, array);
+    }
+
+    [Fact]
+    public void StartingMatchingInput_Find_ShouldReturnCorrectSlice()
+    {
+        const int number = 5;
+        List<int> matchingArray = [2, 3];
+        const int randomNumber = 12;
+
+        List<int> result = SubarraySum.Find(
+            matchingArray.Append(randomNumber).ToList(),
+            number);
+
+        Assert.Equal(matchingArray, result);
+    }
+
+    [Fact]
+    public void TrailingMatchingInput_Find_ShouldReturnCorrectSlice()
+    {
+        const int number = 5;
+        List<int> matchingArray = [2, 3];
+        const int randomNumber = 12;
+
+        List<int> result = SubarraySum.Find(
+            matchingArray.Prepend(randomNumber).ToList(),
+            number);
+
+        Assert.Equal(matchingArray, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Find_Validation(int number, List<int> source, List<int> expected)
+    {
+        List<int> result = SubarraySum.Find(source, number);
+
+        Assert.Equal(expected, result);
+    }
+
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [9, new List<int> { 1, 2, 3, 4, 5 }, new List<int> { 2, 3, 4 }];
+    }
+}
---
+++ SubarraySumEquals/SubarraySum.cs
@@ -0,0 +1,32 @@
+﻿namespace SubarraySumEquals;
+
+public static class SubarraySum
+{
+    public static List<int> Find(List<int> input, int number)
+    {
+        if (input.Count <= 0)
+            return input;
+
+        if (input[0] == number)
+            return input;
+
+        for (int startIndex = 0; startIndex < input.Count; startIndex++)
+        {
+            int sum = 0;
+            for (int i = startIndex; i < input.Count; i++)
+            {
+                sum += input[i];
+
+                if (sum == number)
+                {
+                    return input[startIndex..(i + 1)];
+                }
+                
+                if(sum > number)
+                    break;
+            }
+        }
+
+        return [];
+    }
+}
