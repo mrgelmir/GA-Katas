2d330317bdbaf2c93a8635e8ebb6e0de01c064dc
mrgelmir <sanderdecleer@gmail.com>
2024-07-16 11:23:12 +0200

    Implemented LargestInteger with strings

---
+++ LargestInteger.Tests/IntExtensionsTests.cs
@@ -0,0 +1,61 @@
+namespace LargestInteger.Tests;
+
+public abstract class IntExtensionsTests
+{
+    [Fact]
+    public void EmptyArray_ConstructLargest_ReturnsZero()
+    {
+        int result = IntExtensions.ConstructLargestInt([]);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleNumber_ConstructLargest_ReturnsThatNumber()
+    {
+        const int singleNumber = 10;
+
+        int result = IntExtensions.ConstructLargestInt([singleNumber]);
+
+        Assert.Equal(singleNumber, result);
+    }
+
+    [Fact]
+    public void CorrectlyOrderedNumbers_ConstructLargest_ReturnsConcatenated()
+    {
+        int result = IntExtensions.ConstructLargestInt([5, 1]);
+
+        Assert.Equal(51, result);
+    }
+
+    [Fact]
+    public void ReverseOrderedNumbers_ConstructLargest_ReturnsConcatenated()
+    {
+        int result = IntExtensions.ConstructLargestInt([1, 5]);
+
+        Assert.Equal(51, result);
+    }
+
+    [Fact]
+    public void SameStartingNumberButDifferentNextOne_ConstructLargest_ShouldOrderCorrectly()
+    {
+        int result = IntExtensions.ConstructLargestInt([7, 76]);
+
+        Assert.Equal(776, result);
+    }
+
+    [Fact]
+    public void ConstructLargest_Validate()
+    {
+        int result = IntExtensions.ConstructLargestInt([10, 7, 76, 415]);
+
+        Assert.Equal(77641510, result);
+    }
+
+    protected abstract int Execute(int[] input);
+}
+
+public class StringImplementation1 : IntExtensionsTests
+{
+    protected override int Execute(int[] input) => IntExtensions.StringImplementation1(input);
+}
---
+++ LargestInteger/IntExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace LargestInteger;
+
+public static class IntExtensions
+{
+    public static int ConstructLargestInt(int[] input)
+    {
+        return StringImplementation1(input);
+    }
+
+    public static int StringImplementation1(int[] input)
+    {
+        if (input.Length <= 0)
+            return 0;
+
+        var strings = input.Select(i => i.ToString()).ToList();
+        strings.Sort(SpecificSort);
+
+        return int.Parse(strings.Aggregate("", (a, element) => a += element));
+    }
+
+    private static int SpecificSort(string a, string b)
+    {
+        if (a.Length == b.Length)
+            return -string.Compare(a, b);
+
+        int maxIndex = int.Min(a.Length, b.Length);
+
+        for (int i = 0; i < maxIndex; ++i)
+        {
+            int comparison = b[i] - a[i];
+            if (comparison == 0)
+                continue;
+
+            return comparison;
+        }
+
+        return a.Length - b.Length;
+    }
+}
